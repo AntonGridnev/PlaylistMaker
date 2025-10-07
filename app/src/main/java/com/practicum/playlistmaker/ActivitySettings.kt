0a51f8ad348df493c65f40e1bf188ff15f893c83
Anton Gridnev <anton-grin@ya.ru>
2025-10-07 19:13:48 +0400

    Renamed the icons. Corrected the layout MainActivity.

--- app/src/main/java/com/practicum/playlistmaker/ActivitySettings.kt
+++ app/src/main/java/com/practicum/playlistmaker/ActivitySettings.kt
@@ -23,6 +23,5 @@ class ActivitySettings : AppCompatActivity() {
         backwardSetting.setOnClickListener {
             finish()
         }
-
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -15,7 +15,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                v.paddingLeft + systemBars.left,
+                v.paddingTop + systemBars.top,
+                v.paddingRight + systemBars.right,
+                v.paddingBottom + systemBars.bottom
+            )
             insets
         }
 
