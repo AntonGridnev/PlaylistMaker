3f56bfb2a93dd78fef18d388da298b33fe78d2a2
Anton Gridnev <anton-grin@ya.ru>
2025-10-16 06:22:03 +0400

    1. Реализовал вёрстку SearchAcitivty, опираясь на макет экрана в Figma. 2. Реализовал логику кнопки сброса поисковой строки. 3. Добавил новый TextWatcher для поисковой строки, пока с заглушкой для будущих задач. 4. Установить верные атрибуты для EditText: hint, maxLines и прочие.

--- app/src/main/java/com/practicum/playlistmaker/ActivitySearch.kt
+++ app/src/main/java/com/practicum/playlistmaker/ActivitySearch.kt
@@ -1,6 +1,14 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +24,45 @@ class ActivitySearch : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toolbarSearch = findViewById<Toolbar>(R.id.toolbar_search)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+
+        toolbarSearch.setNavigationOnClickListener  {
+            finish()
+        }
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            hideKeyboard(inputEditText)
+        }
+
+        val simpleTextWatcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //todo
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+            override fun afterTextChanged(s: Editable?) {
+                //todo
+            }
+        }
+
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun hideKeyboard(currentView: View) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(`currentView`.windowToken, 0)
     }
 }
